a54dd00b739593e52baf935e8e8baa686cd718e8
wortis <latre9@yandex.ru>
2022-02-25 15:46:53 +0300

    wow

--- js/gerGoods.js
+++ js/gerGoods.js
@@ -1,21 +1,77 @@
 const getGoods = () => {
-const links = document.querySelectorAll('.navigation-link')
+    const links = document.querySelectorAll('.navigation-link');
+    const viewAll = document.querySelectorAll('.more');
 
-  const getData = () => {
-    fetch('/db/db.json')
-  .then((res) => res.json()
-  )
-  .then((data) => {
-    console.log(data);
-  })
-  }
+    renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
 
-  links.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault()
-    getData()
-  })
-})
+        goodsContainer.innerHTML = '';
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-3');
+
+            goodBlock.innerHTML = `
+                     <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                        <img src="db/${good.img}" alt="${good.label}" class="goods-image">
+						<h3 class="goods-title">${good.name}</h3>
+						<p class="goods-description">${good.description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+            `;
+
+            goodsContainer.append(goodBlock);
+        })
+    }
+
+    const getData = (value, category) => {
+        fetch('https://wildberries-training-project-default-rtdb.firebaseio.com/db.json') 
+        .then((res) => res.json())
+        .then((data) => {
+            const array = category ? data.filter((item) => item[category] === value) : data;
+
+            localStorage.setItem('goods', JSON.stringify(array));
+
+            if (window.location.pathname !== "/goods.html") {
+                window.location.href = 'goods.html';
+            } else {
+                renderGoods(array);
+            }
+        })
+    }
+
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category);
+        })
+    })
+
+    viewAll.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault(); 
+
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category);
+        })
+    })
+
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
+
+
 }
 
-getGoods()
+getGoods();
